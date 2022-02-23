7e74cc6a06072f88496dce8c54003e98a3d955cd
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-02-23 23:52:57 +0600

    register form add

--- hotel/forms.py
+++ hotel/forms.py
@@ -1,5 +1,6 @@
 
 from atexit import register
+from dataclasses import fields
 from tkinter.tix import Form
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -10,4 +11,8 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2'] 
+class CreateRegisterForm(forms.models):
+    class Meta:
+        model = Register
+        fields = ['Id','name']
 

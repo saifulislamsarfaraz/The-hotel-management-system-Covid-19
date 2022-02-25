91e5ddddd1c5a9383ad12dfed897230d44faec09
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-02-26 01:08:39 +0600

    changed

--- HMS/settings.py
+++ HMS/settings.py
@@ -140,3 +140,11 @@ MEDIA_URL = 'images/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sarfrazahmed42654@gmail.com'
+EMAIL_HOST_PASSWORD = 'S@if42654'

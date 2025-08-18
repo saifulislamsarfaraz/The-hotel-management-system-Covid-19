71ce0f8a7ddee600539466dc980a64e1b2a92f41
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2025-08-18 14:24:00 +0600

    Clean up: Remove unused imports from HMS/urls.py.

--- HMS/urls.py
+++ HMS/urls.py
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import views
 from django.contrib import admin
 from django.urls import include, path
-from django.http import HttpResponse
 from django.conf.urls.static import static
 from django.conf import settings
 

60340e1711c49b661b39ee57d5c4e7a16c4de6af
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2025-08-18 14:12:09 +0600

    Improve security: Remove hardcoded SECRET_KEY and ensure DEBUG defaults to False.

--- HMS/settings.py
+++ HMS/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-b&5@2&1@dy8)&mwpm&(6_dpwqg&$rl4f_()vq1!@ycb3^3^*l@')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 

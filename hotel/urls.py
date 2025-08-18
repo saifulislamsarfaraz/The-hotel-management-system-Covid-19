f215be69940d1b2cf0d7b00ab8f07916b82cd822
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2025-08-18 14:12:45 +0600

    Clean up: Remove unused imports from hotel/urls.py.

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,13 +1,9 @@
-from unicodedata import name
 from django.urls import path
 
-from hotel.models import RoomCategory
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from employeeRec import urls
-from healthcare import urls
 
 
 
